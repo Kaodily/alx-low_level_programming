5e2138d5aaf54fbdfd37981ec0af969cad2b715b
Kaodily <isizohchika@gmail.com>
2023-03-17 08:02:20 -0700

    loop reverse' clear exit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 26; i > 0; i--)
+	{
+		putchar(alphabets[i]);
+	}
+	putchar('\n');
+	return (0);
+}
