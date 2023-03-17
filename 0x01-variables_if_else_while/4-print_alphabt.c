e8ecec34564898adc18acf41cd2efa5458593847
Kaodily <isizohchika@gmail.com>
2023-03-17 07:51:12 -0700

    loops

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (alphabets[i] == 'q' || alphabets[i] == 'e')
+			break;
+			putchar(alphabets[i]); }
+	putchar('\n');
+	return (0);
+}
+
