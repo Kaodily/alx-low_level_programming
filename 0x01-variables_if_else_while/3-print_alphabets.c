813d79195c9afc533ee5d89c17b779851adfe1a3
Kaodily <isizohchika@gmail.com>
2023-03-17 14:52:44 -0700

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,18 +8,12 @@ int main(void)
 {
 	char alphaS[26] = "abcdefghijklmnopqrstuvwxyz";
 	char alphaC[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
-
-	int j;
+	int i, j;
 
 	for (i = 0; i < 26; i++)
-	{
 		putchar(alphaS[i]);
-		for (j = 0; j < 26; j++)
-		{
-			putchar(alphaC[j]);
-		}
-	}
+	for (j = 0; j < 26; j++)
+		putchar(alphaC[j]);
 	putchar('\n');
 	return (0);
 }
